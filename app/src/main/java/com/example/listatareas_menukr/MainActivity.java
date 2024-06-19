79355509e49cc41f6eecb0b4c4bc6d3ef17047c3
Karitz <krnrodea@gmail.com>
2024-06-19 07:38:48 -0600

    Segundo commit

--- app/src/main/java/com/example/listatareas_menukr/MainActivity.java
+++ app/src/main/java/com/example/listatareas_menukr/MainActivity.java
@@ -1,24 +1,90 @@
 package com.example.listatareas_menukr;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonMenu;
+    EditText editar;
+    ListView lista;
+    ArrayList<String> Tareas;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        lista = findViewById(R.id.List);
+        editar = findViewById(R.id.Edit);
+        buttonMenu = findViewById(R.id.Boton);
+        registerForContextMenu(buttonMenu);
+
+        Tareas = new ArrayList<>();
+
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Tareas);
+        lista.setAdapter(adapter);
+
+
     }
+
+
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+            if (id== R.id.Agregar) {
+                añadir();
+            }
+            else if (id == R.id.Edit){
+                editar();
+            }
+            else if (id == R.id.Eliminar) {
+                eliminar();
+            }
+            return super.onContextItemSelected(item);
+    }
+
+    private void añadir() {
+        String tar = editar.getText().toString().trim();
+
+        if (!tar.isEmpty()) {
+            int selectedTaskPosition = lista.getCheckedItemPosition();
+
+            if (selectedTaskPosition != AdapterView.INVALID_POSITION) {
+                Tareas.set(selectedTaskPosition, tar);
+            } else {
+                Tareas.add(tar);
+            }
+            adapter.notifyDataSetChanged();
+            editar.setText("");
+        }
+
+    }
+
+    private void editar() {
+
+    }
+
+    private void eliminar() {
+    }
+
 }
